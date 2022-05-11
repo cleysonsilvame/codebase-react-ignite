471cb110feb884303c97b4017ae77bc4fa2d7c25
Cleyson Silva <cleysonsilva.me@gmail.com>
2022-05-11 04:42:43 -0300

    chapter-II: Consumindo API

--- chapter-2/src/App.tsx
+++ chapter-2/src/App.tsx
@@ -1,6 +1,26 @@
+import { createServer } from "miragejs";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
+
+createServer({
+  routes() {
+    this.namespace = "api";
+
+    this.get("/transactions", () => {
+      return [
+        {
+          id: 1,
+          title: "Transaction 1",
+          amount: 100,
+          type: 'deposit',
+          category: 'Food',
+          createdAt: new Date()
+        }
+      ];
+    });
+  },
+});
 
 export function App() {
   return (
