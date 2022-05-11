1567f9c0265a4e6229521cc3b5f7f4e98f10438f
Cleyson Silva <cleysonsilva.me@gmail.com>
2022-05-11 04:33:56 -0300

    chapter-II: Componentização

--- chapter-2/src/styles/global.ts
+++ chapter-2/src/styles/global.ts
@@ -3,6 +3,7 @@ import { createGlobalStyle } from "styled-components";
 export const GlobalStyle = createGlobalStyle`
   :root {
     --red: #E52E4D;
+    --green: #33CC95;
     --blue: #5429CC;
     
     --blue-light: #6933FF;
@@ -28,7 +29,7 @@ export const GlobalStyle = createGlobalStyle`
     }
   }
 
-  body: {
+  body {
     background: var(--background);
     --webkit-font-smoothing: antialiased;
   }
