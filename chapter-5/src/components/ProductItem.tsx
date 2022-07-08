a3e27f94624eb47b697117f22e86d42ce2349e3b
Cleyson Silva <cleysonsilva.me@gmail.com>
2022-07-08 07:51:01 -0300

    chapter-V: Otimizações

--- chapter-5/src/components/ProductItem.tsx
+++ chapter-5/src/components/ProductItem.tsx
@@ -1,15 +1,44 @@
+import { memo, useState } from "react";
+import dynamic from "next/dynamic";
+import { AddProductToWishListProps } from "./AddProductToWishlist";
+import lodash from 'lodash'
+
+const AddProductToWishlist = dynamic<AddProductToWishListProps>(async () => {
+  const mod = await import("./AddProductToWishlist");
+  return mod.AddProductToWishlist;
+});
+
 interface ProductItemProps {
   product: {
     id: number;
     price: number;
     title: string;
   };
+  onAddToWishlist: (id: number) => void;
 }
 
-export function ProductItem({ product }: ProductItemProps) {
+function ProductItemComponent({ product, onAddToWishlist }: ProductItemProps) {
+  const [isAddingToWishlist, setIsAddingToWishlist] = useState(false);
+
   return (
     <div>
       {product.title} - <strong>{product.price}</strong>
+      <button onClick={() => setIsAddingToWishlist(true)}>
+        Adicionar aos favoritos
+      </button>
+      {isAddingToWishlist && (
+        <AddProductToWishlist
+          onAddToWishlist={() => onAddToWishlist(product.id)}
+          onRequestClose={() => setIsAddingToWishlist(false)}
+        />
+      )}
     </div>
   );
 }
+
+export const ProductItem = memo(
+  ProductItemComponent,
+  (prevProps, nextProps) => {
+    return lodash.isEqual(prevProps.product, nextProps.product);
+  }
+);
--- chapter-5/src/pages/index.tsx
+++ chapter-5/src/pages/index.tsx
@@ -1,9 +1,9 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { SearchResults } from "../components/SearchResults";
 
 export default function Home() {
   const [search, setSearch] = useState("");
-  const [results, setResults] = useState([]);
+  const [results, setResults] = useState({ data: [], totalPrice: 0 });
 
   async function handleSearch(e: React.FormEvent) {
     e.preventDefault();
@@ -13,8 +13,19 @@ export default function Home() {
     const response = await fetch(`http://localhost:3333/products?q=${search}`);
     const data = await response.json();
 
-    setResults(data);
+    const totalPrice = data.reduce(
+      (total: number, product: { price: number }) => {
+        return total + product.price;
+      },
+      0
+    );
+
+    setResults({ data, totalPrice });
   }
+
+  const addToWishlist = useCallback(async (id: number) => {
+    console.log(id);
+  }, []);
 
   return (
     <div>
@@ -30,7 +41,11 @@ export default function Home() {
         <button type="submit">Buscar</button>
       </form>
 
-      <SearchResults results={results} />
+      <SearchResults
+        results={results.data}
+        totalPrice={results.totalPrice}
+        onAddToWishlist={addToWishlist}
+      />
     </div>
   );
 }
